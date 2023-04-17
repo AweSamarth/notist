bfd1089c8d6760e227404297d06ebd18a6cf2e13
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-04-17 22:58:05 +0530

    mid finals week ship lmao

--- pages/components/Explainer.tsx
+++ pages/components/Explainer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import {
+  AnimatePresence,
   motion,
   MotionValue,
   useMotionValue,
@@ -18,16 +19,22 @@ export default function Hero() {
   const [what, setWhat] = useState(true);
   const [why, setWhy] = useState(false);
   const [how, setHow] = useState(false);
+  const [part, setPart] = useState(-1);
 
-  const ref = useRef(null);
-  const { scrollXProgress } = useScroll({ container: ref });
-
-  const { scrollYProgress } = useScroll({ container: ref });
-
-  const y = useParallax(scrollYProgress, 10);
+  const { scrollYProgress } = useScroll();
 
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     console.log("Container scroll: ", latest);
+    if (latest < 0.5) {
+      setPart(1);
+      console.log(part);
+    } else if (latest >= 0.5 && latest < 0.6) {
+      setPart(2);
+      console.log(part);
+    } else if (latest >= 0.6) {
+      setPart(3);
+      console.log(part);
+    }
   });
 
   function useParallax(value: MotionValue<number>, distance: number) {
@@ -36,78 +43,67 @@ export default function Hero() {
   }
 
   return (
-    <>
-
-      <svg id="progress" width="100" height="100" viewBox="0 0 100 100">
-        <circle
-          cx="50"
-          cy="50"
-          r="30"
-          pathLength="1"
-          className=" stroke-[#fafafa] opacity-[30%]"
-        />
-        <motion.circle
-          cx="50"
-          cy="50"
-          r="30"
-          pathLength="1"
-          className="indicator"
-          style={{ pathLength: scrollYProgress }}
-        />
-      </svg>
-      <main
-        ref={ref}
-        className="h-screen  border-2 overflow-y-visible relative border-red-500 w-full"
-      >
+    <AnimatePresence>
+      <main className=" h-max  border-2 overflow-y-visible relative border-red-500 w-full">
         <div className="min-h-screen flex flex-col justify-center  border-2 overflow-y-visible">
-          <motion.div
-            className="border- border-red-500 text-center overflow-y-visible flex flex-col gap-3 items-center "
-            style={{ y }}
-            animate={why ? { y: -100, opacity: 0 } : {}}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          >
-            <div
-              className="text-[4.2rem] font-manrope font-light h-max     w-max text-transparent 
-    bg-clip-text bg-gradient-to-br from-[#2E5CD0] to-[#384564]"
+          {part === 1 || part === -1 ? (
+            <motion.div
+              key={1}
+              className="border-2 border-red-500 text-center overflow-y-visible flex flex-col gap-3 items-center "
+              initial={{ opacity: 0 }}
+              animate={{ y: -50, opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              exit={{ y: -100, opacity: 0 }}
             >
-              <span className="font-semibold bg-[#2E5CD0] text-transparent bg-clip-text">
-                10x
-              </span>{" "}
-              your learning speed
-            </div>
-            <div className=" text-[#B1B6E1] font-inter text-3xl font-light">
-              why watch in 2x when you can learn in 10x?
-            </div>
-          </motion.div>
-
-          <div
-            onClick={() => setWhy((prev) => !prev)}
-            className="text-black text-center bg-white"
-          >
-            button
-          </div>
+              <div
+                className="text-[4.2rem] font-manrope font-light h-max     w-max text-transparent 
+    bg-clip-text bg-gradient-to-br from-[#2E5CD0] to-[#384564]"
+              >
+                <span className="font-semibold bg-[#2E5CD0] text-transparent bg-clip-text">
+                  10x
+                </span>{" "}
+                your learning speed
+              </div>
+              <div className=" text-[#B1B6E1] font-inter text-3xl font-light">
+                why watch in 2x when you can learn in 10x?
+              </div>
+            </motion.div>
+          ) : part === 2 ? (
+            <motion.div
+              key={2}
+              className="border-2 border-red-500 text-center overflow-y-visible flex flex-col gap-3 items-center "
+              initial={{ opacity: 0 }}
+              animate={{ y: -50, opacity: 1 }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              exit={{ y: -100, opacity: 0 }}
+            >
+              testing it out
+            </motion.div>
+          ) : (
+            <motion.div
+              key={3}
+              className="border-2 border-red-500 text-center overflow-y-visible flex flex-col gap-3 items-center "
+              initial={{ opacity: 0 }}
+              animate={{ y: -50, opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              exit={{ y: -100, opacity: 0 }}
+            >
+              <div
+                className="text-[4.2rem] font-manrope font-light h-max     w-max text-transparent 
+    bg-clip-text bg-gradient-to-br from-[#2E5CD0] to-[#384564]"
+              >
+                <span className="font-semibold bg-[#2E5CD0] text-transparent bg-clip-text">
+                  10x
+                </span>{" "}
+                your coughinh speed
+              </div>
+              <div className=" text-[#B1B6E1] font-inter text-3xl font-light">
+                why cough cough in 2x when you can cough cough cough cough?
+              </div>
+            </motion.div>
+          )}
         </div>
       </main>
-      <ul
-        className="border-2 border-red-500 flex flex-col list-none h-[200px] overflow-y-scroll p-5 w-full m-0"
-        ref={ref}
-      >
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-
-        <li className="bg-white text-black">ds</li>
-
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-        <li className="bg-white text-black">ds</li>
-      </ul>
-    </>
+    </AnimatePresence>
   );
 }
